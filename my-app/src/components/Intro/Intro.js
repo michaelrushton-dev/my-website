571ee89128b9eedf126784012ae93ba3a1892a64
michaelrushton-dev <michaelwjrushton@gmail.com>
2022-07-19 15:52:47 +0100

    got arrow transparency working with scroll position

--- my-app/src/components/Intro/Intro.js
+++ my-app/src/components/Intro/Intro.js
@@ -3,10 +3,10 @@ import "./intro.css";
 import "../../App.css";
 
 function Intro({ scrollPosition }) {
-    let opacityScroll = 1;
+    let opacityScroll;
     //NEED TO FIX THIS SO THE ARROW OPACITY INCREASES WHEN SCROLL POSITION IS ABOVE 300
-    if (scrollPosition > 300) {
-        opacityScroll = scrollPosition / 1000;
+    if (scrollPosition) {
+        opacityScroll = 1 - scrollPosition / 150;
     }
     return (
         <div className="intro">
