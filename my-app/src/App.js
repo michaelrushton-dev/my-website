e94e84105bf2686f122b4ee5f45dc4043a4a57d1
michaelrushton-dev <michaelwjrushton@gmail.com>
2022-08-18 11:36:13 +0100

    media queries added to navbar and about section

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,22 +1,22 @@
-import "./App.css";
-import Intro from "./components/Intro/Intro.js";
-import Navbar from "./components/Navbar/Navbar.js";
-import About from "./components/About/About.js";
-import Projects from "./components/Projects/Projects.js";
-import Contact from "./components/Contact/Contact.js";
-import Footer from "./components/Footer/Footer.js";
-import useScroll from "./hooks/useScroll.js";
+import './App.css';
+import Intro from './components/Intro/Intro.js';
+import Navbar from './components/Navbar/Navbar.js';
+import About from './components/About/About.js';
+import Projects from './components/Projects/Projects.js';
+import Contact from './components/Contact/Contact.js';
+import Footer from './components/Footer/Footer.js';
+import useScroll from './hooks/useScroll.js';
 
 function App() {
     // custom hook to capture scroll position for styling
     const { scrollPosition } = useScroll();
-    console.log(scrollPosition);
+    // console.log(scrollPosition);
     // custom hook to capture scroll position for styling
 
     return (
-        <div className="App">
+        <div className='App'>
             <Navbar scrollPosition={scrollPosition} />
-            <div className="wrapper">
+            <div className='wrapper'>
                 {/* wrapper is only to seperate sticky nav from rest of app */}
                 <Intro scrollPosition={scrollPosition} />
                 <About />
--- my-app/src/components/About/About.js
+++ my-app/src/components/About/About.js
@@ -1,21 +1,38 @@
-import React from "react";
-import "./about.css";
-import "../../App.css";
+import React from 'react';
+import './about.css';
+import '../../App.css';
 
 function About() {
     return (
-        <div id="about">
-            <div className="inner-content" style={{ paddingTop: "8em" }}>
+        <div id='about'>
+            <div className='inner-content'>
                 <h2> A bit about me... </h2>
-                <p className="about-me">
-                    aoishdalishcliasnisanais kaushdunuxnaoixoianoxinaixnaw
-                    kaushdunuxnaoixoianoxinaixnaw kaushdunuxnaoixoianoxinaixnaw
-                    kaushdunuxnaoixoianoxinaixnaw kaushdunuxnaoixoianoxinaixnaw
-                    kaushdunuxnaoixoianoxinaixnaw kaushdunuxnaoixoianoxinaixnaw
-                    kaushdunuxnaoixoianoxinaixnaw kaushdunuxnaoixoianoxinaixnaw
-                    kaushdunuxnaoixoianoxinaixnaw kaushdunuxnaoixoianoxinaixnaw
-                    kaushdunuxnaoixoianoxinaixnaw
+                <br />
+                <p className='about-me'>
+                    Nice to meet you, i'm Mike Rushton.
+                    <br />
+                    <br /> I have taken the leap out of my career as an optician
+                    working in the Cayman Islands into an amazing new world of
+                    tech where I learn new things each day and meet engaging,
+                    forward-thinking people...pretty good choice if you ask me!
+                    <br />
+                    <br /> Having always been obsessed with learning about other
+                    people and their cultures I have been blown away by the
+                    diverse and inclusive community I have become part of
+                    through attending meet-ups and through my incredible
+                    bootcamp: the School of Code.
+                    <br />
+                    <br /> I owe the School of Code so much. I have not only
+                    become a full stack developer in three months, but also
+                    become seasoned in Agile methodology and team work.
+                    <br />
+                    <br /> The bootcamp is like a job. We carry out daily
+                    stand-ups and retros, always work together through
+                    pair-programming or mob-programming, and are part of a
+                    helpful community that has a vibrant approach to encouraging
+                    eachother's progress.
                 </p>
+                <div className='tech-stack'></div>
             </div>
         </div>
     );
