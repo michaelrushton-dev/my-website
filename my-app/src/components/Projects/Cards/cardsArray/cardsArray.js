d5c63135228a1dcd8cb806ba13e18711c20574d8
michaelrushton-dev <michaelwjrushton@gmail.com>
2022-08-21 12:47:30 +0100

    navbar links on hover styling

--- my-app/src/components/Projects/Cards/cardsArray/cardsArray.js
+++ my-app/src/components/Projects/Cards/cardsArray/cardsArray.js
@@ -1,9 +1,12 @@
-import card1 from './project-card-1.png';
+import card1 from './Medi-Check.png';
 import card2 from './project-card-2.png';
 import card3 from './project-card-3.png';
 import card4 from './project-card-4.png';
 import card5 from './project-card-5.png';
 
 let cardArray = [card1, card2, card3, card4, card5];
+let linksArray = [
+    'https://github.com/SchoolOfCode/final-project_front-end-five-guys',
+];
 
 export default cardArray;
