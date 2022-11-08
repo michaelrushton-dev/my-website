473bad824695f6cd0a35a69a6d16f5bf0ccaecce
michaelrushton-dev <michaelwjrushton@gmail.com>
2022-11-08 15:49:56 +0000

    Added dartboard app to projects list

--- my-app/src/components/Projects/Cards/Descriptions/descriptionsArray.js
+++ my-app/src/components/Projects/Cards/Descriptions/descriptionsArray.js
@@ -1,5 +1,6 @@
 const desc1 = `My team's final project at School of Code helps avoid dangerous prescription clashes as well as storing vital healthcare information. It has both GP and patient GUIs`;
-const desc2 = 'This is a description of the second project called Wack';
+const desc2 =
+    "Dynamically keeps track of the user's darts score and winning status";
 const desc3 = 'This is a description of the third project called Sandwich';
 const desc4 = 'This is a description of the fourth project called Taco';
 const desc5 = 'This is a description of the fifth project called Enchillada';
--- my-app/src/components/Projects/Cards/cardsArray/cardsArray.js
+++ my-app/src/components/Projects/Cards/cardsArray/cardsArray.js
@@ -1,5 +1,5 @@
 import card1 from './Medi-Check.png';
-import card2 from './project-card-2.png';
+import card2 from './Dartboard.png';
 import card3 from './project-card-3.png';
 import card4 from './project-card-4.png';
 import card5 from './project-card-5.png';
@@ -7,4 +7,5 @@ import card5 from './project-card-5.png';
 export let cardArray = [card1, card2, card3, card4, card5];
 export let linksArray = [
     'https://github.com/SchoolOfCode/final-project_front-end-five-guys',
+    'https://github.com/michaelrushton-dev/darts-game',
 ];
