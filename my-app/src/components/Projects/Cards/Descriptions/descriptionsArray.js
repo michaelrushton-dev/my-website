3e379c2bd30314f972551188b22ecdbbfea0397c
michaelrushton-dev <michaelwjrushton@gmail.com>
2022-12-01 01:41:22 +0000

    updated CV download and added in current projects

--- my-app/src/components/Projects/Cards/Descriptions/descriptionsArray.js
+++ my-app/src/components/Projects/Cards/Descriptions/descriptionsArray.js
@@ -1,8 +1,8 @@
 const desc1 = `My team's final project at School of Code helps avoid dangerous prescription clashes as well as storing vital healthcare information. It has both GP and patient GUIs`;
 const desc2 =
     "Dynamically keeps track of the user's darts score and winning status";
-const desc3 = 'This is a description of the third project called Sandwich';
-const desc4 = 'This is a description of the fourth project called Taco';
+const desc3 = 'A retro style sample pad made with Web Audio API';
+const desc4 = 'A charity app to connect users with food banks';
 const desc5 = 'This is a description of the fifth project called Enchillada';
 
 const descriptionArray = [desc1, desc2, desc3, desc4, desc5];
--- my-app/src/components/Projects/Cards/cardsArray/cardsArray.js
+++ my-app/src/components/Projects/Cards/cardsArray/cardsArray.js
@@ -1,11 +1,13 @@
 import card1 from './Medi-Check.png';
 import card2 from './Dartboard.png';
-import card3 from './project-card-3.png';
-import card4 from './project-card-4.png';
+import card3 from './JamDeck.png';
+import card4 from './Community-Yard.jpg';
 import card5 from './project-card-5.png';
 
 export let cardArray = [card1, card2, card3, card4, card5];
 export let linksArray = [
     'https://github.com/SchoolOfCode/final-project_front-end-five-guys',
     'https://github.com/michaelrushton-dev/darts-game',
+    'https://github.com/AlphaPentagon/js-drumkit',
+    'https://github.com/St0neofFr33dom/18-11-Hackathon',
 ];
